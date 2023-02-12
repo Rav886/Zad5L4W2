f678e2e3466a941d02831e35c3b1c59641258445
Ra v <timon521182@hotmail.com>
2023-02-12 23:00:26 +0100

    Add project files.

---
+++ Zad5L4W2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname, adress, born, namedad, namemam, surnamemam, education, employment, mail;
+            int number, height;
+            double wight;
+
+            Console.WriteLine("Witaj w programie pobierającym Twoje dane");
+            
+            Console.WriteLine("Podaj swoje imie");            
+            name = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoje nazwisko");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoj adres");
+            adress = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoje miejsce urodzenia");
+            born = Console.ReadLine();
+
+            Console.WriteLine("Podaj imie ojca");
+            namedad = Console.ReadLine();
+
+            Console.WriteLine("Podaj imie matki");
+            namemam = Console.ReadLine();
+
+            Console.WriteLine("Podaj nazwisko panieńskie matki");
+            surnamemam = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoje wykrztałcenie");
+            education = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoje miejsce zatrudnienia");
+            employment = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoj adres mail");
+            mail = Console.ReadLine();
+
+            Console.WriteLine("Podaj swoj numer telefonu");
+            number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj swoj wzrost");
+            height = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Podaj swoją wage");
+            wight = double.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
